4ff15a41eaabc4f9a4b7e333411b5da63ce50415
heover1cks <ubuntu@DESKTOP-PTHEET2.localdomain>
2021-01-11 16:07:24 +0900

    todo with context

---
+++ mashup-todolist/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import { TodoProvider } from "./components/TodoContext";
+import TodoCreate from "./components/TodoCreate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoTemplate from "./components/TodoTemplate";
+
+const GlobalStyle = createGlobalStyle`
+body{
+  background: #e9ecef;
+}
+`;
+
+const App = () => {
+  return (
+    <>
+      <TodoProvider>
+        <GlobalStyle />
+        <TodoTemplate>
+          <TodoHead />
+          <TodoList />
+          <TodoCreate />
+        </TodoTemplate>
+      </TodoProvider>
+    </>
+  );
+};
+
+export default App;
